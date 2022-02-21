fdb6573ae9ce86fd4124b573d8ee219835553b25
Lihsayuri <liviasm1@al.insper.edu.br>
2022-02-21 17:44:38 -0300

    Falta última parte do LAB

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -1,17 +1,17 @@
 /**
- * 5 semestre - Eng. da Computação - Insper
- * Rafael Corsi - rafael.corsi@insper.edu.br
- *
- * Projeto 0 para a placa SAME70-XPLD
- *
- * Objetivo :
- *  - Introduzir ASF e HAL
- *  - Configuracao de clock
- *  - Configuracao pino In/Out
- *
- * Material :
- *  - Kit: ATMEL SAME70-XPLD - ARM CORTEX M7
- */
+* 5 semestre - Eng. da Computação - Insper
+* Rafael Corsi - rafael.corsi@insper.edu.br
+*
+* Projeto 0 para a placa SAME70-XPLD
+*
+* Objetivo :
+*  - Introduzir ASF e HAL
+*  - Configuracao de clock
+*  - Configuracao pino In/Out
+*
+* Material :
+*  - Kit: ATMEL SAME70-XPLD - ARM CORTEX M7
+*/
 
 /************************************************************************/
 /* includes                                                             */
@@ -19,14 +19,25 @@
 
 #include "asf.h"
 
+
 /************************************************************************/
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO           PIOC                 // periferico que controla o LED
+// #
+#define LED_PIO_ID        ID_PIOC                 // ID do periférico PIOC (controla LED)
+#define LED_PIO_IDX       8                    // ID do LED no PIO
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
+#define BUT_PIO PIOA
+#define BUT_PIO_ID ID_PIOA
+#define BUT_PIO_IDX 11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+
+
+
+
 
 /************************************************************************/
 /* constants                                                            */
@@ -53,6 +64,29 @@ void init(void);
 // Função de inicialização do uC
 void init(void)
 {
+	// Initialize the board clock
+	sysclk_init();
+
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED.
+	pmc_enable_periph_clk(LED_PIO_ID);
+	
+	// Inicializa PIO do botao
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	
+	//Inicializa PC8 como saída
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	
+	// configura pino ligado ao botão como entrada com um pull-up.
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	
+	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 0);
+	
+	
+	
 
 }
 
@@ -63,13 +97,27 @@ void init(void)
 // Funcao principal chamada na inicalizacao do uC.
 int main(void)
 {
-  init();
+	init();
 
-  // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
-  while (1)
-  {
+	// super loop
+	// aplicacoes embarcadas não devem sair do while(1).
+	
+	while(1){
+		int resultPio;
+		resultPio = pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK);
 
-  }
-  return 0;
+		if (resultPio == 0){
+			for (int i = 0; i < 5; i++ )
+			{
+				pio_set(PIOC, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
+				delay_ms(100);                        // Delay por software de 200 ms
+				pio_clear(PIOC, LED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+				delay_ms(100);                        // Delay por software de 200 ms
+			} 
+			
+			pio_set(PIOC, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
+		}
+	}
+	return 0;
 }
+
